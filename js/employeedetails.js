0a271ab3bea0cf1857b092e0d041efda5260fe97
canariasenmoto <hacomar@canariasenmoto.com>
2015-12-30 14:14:34 +0000

    AppCM v4

--- js/employeedetails.js
+++ js/employeedetails.js
@@ -5,6 +5,22 @@ $('#detailsPage').live('pageshow', function(event) {
 	displayEmployee(id);
 });
 
+
+$('#detailsPageEvento').live('pageshow', function(event) {
+	var id = getUrlVars()["id"];
+	//$.getJSON(serviceURL, displayEmployee);
+	//$.getJSON(serviceURL + 'getemployee.php?id='+id, displayEmployee);
+	displayEvento(id);
+});
+
+
+$('#detailsPageOcasion').live('pageshow', function(event) {
+	var id = getUrlVars()["id"];
+	//$.getJSON(serviceURL, displayEmployee);
+	//$.getJSON(serviceURL + 'getemployee.php?id='+id, displayEmployee);
+	displayOcasion(id);
+});
+
 function displayEmployee(id) { 
 	//var employee = data.item;
 	//console.log(employee);
@@ -62,6 +78,125 @@ function displayEmployee(id) {
 	
 	
 }
+
+
+function displayEvento(id) { 
+	//var employee = data.item;
+	//console.log(employee);
+	var deviceWidth;
+	var serviceURL3 = "http://tienda.canariasenmoto.com/servicecm/evento.json?id="+id;
+	deviceWidth = $('[data-role="page"]').first().width();
+	$.getJSON(serviceURL3, function(data) {
+		$.each(data, function(index, evento) {
+	
+			$('#fullName').text(evento.titular_portada);
+			$('#eventoName').append('<p>' + evento.titular + '</p>');
+			$('#employeePic').attr('src', evento.ruta_imagen);
+			$('#employeePic').attr('width', deviceWidth);
+			$('#localizacion').append(evento.lugar);
+			$('#fechaevento').append(evento.fechaevento);
+			$('#eventoDescripcion').append('<p id="descripcionevento"><span class="tituloseccion">DETALLE DEL EVENTO</span><br>' + evento.texto + '</p>');			
+			
+			$('#eventoMotoclubs').append('<p>' + evento.motoclubs + '</p>');
+		    
+			$('#actionList').append('<li><a href="' + evento.enlace + '"><h3>Ver en la web</h3></a></li>');
+			$('#actionList').append('<li><a href="' + evento.enlace + '"><h3>Compartir</h3></a></li>');
+			
+			/*
+			$('#fullName').text(employee.firstName + ' ' + employee.lastName);
+			$('#employeeTitle').text(employee.title);
+			$('#city').text(employee.city);
+			console.log(employee.officePhone);
+			if (employee.managerId>0) {
+				$('#actionList').append('<li><a href="employeedetails.html?id=' + employee.managerId + '"><h3>View Manager</h3>' +
+						'<p>' + employee.managerFirstName + ' ' + employee.managerLastName + '</p></a></li>');
+			}
+			if (employee.reportCount>0) {
+				$('#actionList').append('<li><a href="reportlist.html?id=' + employee.id + '"><h3>View Direct Reports</h3>' +
+						'<p>' + employee.reportCount + '</p></a></li>');
+			}
+			if (employee.email) {
+				$('#actionList').append('<li><a href="mailto:' + employee.email + '"><h3>Email</h3>' +
+						'<p>' + employee.email + '</p></a></li>');
+			}
+			if (employee.officePhone) {
+				$('#actionList').append('<li><a href="tel:' + employee.officePhone + '"><h3>Call Office</h3>' +
+						'<p>' + employee.officePhone + '</p></a></li>');
+			}
+			if (employee.cellPhone) {
+				$('#actionList').append('<li><a href="tel:' + employee.cellPhone + '"><h3>Call Cell</h3>' +
+						'<p>' + employee.cellPhone + '</p></a></li>');
+				$('#actionList').append('<li><a href="sms:' + employee.cellPhone + '"><h3>SMS</h3>' +
+						'<p>' + employee.cellPhone + '</p></a></li>');
+			}*/
+		});
+		$('#actionList').listview('refresh');
+	
+	});
+	
+	
+}
+
+
+function displayOcasion(id) { 
+	//var employee = data.item;
+	//console.log(employee);
+	var deviceWidth;
+	var serviceURL2 = "http://tienda.canariasenmoto.com/servicecm/motoocasion.json?id="+id;
+	deviceWidth = $('[data-role="page"]').first().width();
+	$.getJSON(serviceURL2, function(data) {
+		$.each(data, function(index, ocasion) {
+	
+			$('#fullName').text(ocasion.modelo);
+			$('#modeloOcasion').append('<p>' + ocasion.modelo + '</p>');		
+			$('#employeePic').attr('src', ocasion.ruta_imagen);
+			$('#employeePic').attr('width', deviceWidth);
+			$('#kilometros').text(ocasion.kilometros + ' km');
+			$('#precioocasion').append(ocasion.precio);
+			$('#descripcionoferta').append(ocasion.descripcion);
+			$('#datosempresa').append('<p><strong>' + ocasion.nombre_empresa + '</strong></p>');	
+			
+			if (ocasion.telefono) {
+				$('#actionList').append('<li><a href="tel:+34' + ocasion.telefono + '"><h3>Tel&eacute;fono</h3>' +
+						'<p>' + ocasion.telefono + '</p></a></li>');
+				$('#actionList').append('<li><a href="mailto:' + ocasion.email + '"><h3>Correo electr&oacute;nico</h3>' +
+						'<p>' + ocasion.email + '</p></a></li>');
+			}
+			/*
+			$('#fullName').text(employee.firstName + ' ' + employee.lastName);
+			$('#employeeTitle').text(employee.title);
+			$('#city').text(employee.city);
+			console.log(employee.officePhone);
+			if (employee.managerId>0) {
+				$('#actionList').append('<li><a href="employeedetails.html?id=' + employee.managerId + '"><h3>View Manager</h3>' +
+						'<p>' + employee.managerFirstName + ' ' + employee.managerLastName + '</p></a></li>');
+			}
+			if (employee.reportCount>0) {
+				$('#actionList').append('<li><a href="reportlist.html?id=' + employee.id + '"><h3>View Direct Reports</h3>' +
+						'<p>' + employee.reportCount + '</p></a></li>');
+			}
+			if (employee.email) {
+				$('#actionList').append('<li><a href="mailto:' + employee.email + '"><h3>Email</h3>' +
+						'<p>' + employee.email + '</p></a></li>');
+			}
+			if (employee.officePhone) {
+				$('#actionList').append('<li><a href="tel:' + employee.officePhone + '"><h3>Call Office</h3>' +
+						'<p>' + employee.officePhone + '</p></a></li>');
+			}
+			if (employee.cellPhone) {
+				$('#actionList').append('<li><a href="tel:' + employee.cellPhone + '"><h3>Call Cell</h3>' +
+						'<p>' + employee.cellPhone + '</p></a></li>');
+				$('#actionList').append('<li><a href="sms:' + employee.cellPhone + '"><h3>SMS</h3>' +
+						'<p>' + employee.cellPhone + '</p></a></li>');
+			}*/
+		});
+		$('#actionList').listview('refresh');
+	
+	});
+	
+	
+}
+
 
 function getUrlVars() {
     var vars = [], hash;
--- js/employeelist.js
+++ js/employeelist.js
@@ -1,12 +1,18 @@
 var serviceURL = "http://tienda.canariasenmoto.com/servicecm/destacados.json";
+var serviceeventosURL = "http://tienda.canariasenmoto.com/servicecm/eventos.json";
+var serviceocasionURL = "http://tienda.canariasenmoto.com/servicecm/ofertas.json";
 
 var employees;
 
 $('#employeeListPage').bind('pageinit', function(event) {
-	getEmployeeList();
+	getOfertasList();
 });
 
-function getEmployeeList() {
+
+
+
+function getOfertasList() {
+	mostrarOfertas();
 	$.getJSON(serviceURL, function(data) {
 		$('#employeeList li').remove();
 		employees = data;
@@ -15,8 +21,66 @@ function getEmployeeList() {
 					'<img src="' + employee.ruta_imagen + '"/>' +
 					'<h4>' + employee.nombre + '</h4>' +
 					'<p>' + employee.nombre_empresa + '</p>' +
-					'<span class="ui-li-count">' + employee.precio_oferta + '</span></a></li>');
+					'<span class="precioantes">' + employee.precio + ' &euro;</span><span class="preciooferta">' + employee.precio_oferta + ' &euro;</span></a></li>');
 		});
 		$('#employeeList').listview('refresh');
 	});
+}
+
+
+function getEventosList() {
+	mostrarEventos();
+	$.getJSON(serviceeventosURL, function(data) {
+		$('#eventosList li').remove();
+		eventos = data;
+		$.each(eventos, function(index, evento) {
+			$('#eventosList').append('<li><a href="eventodetails.html?id=' + evento.id_noticia + '">' +
+					'<img src="' + evento.ruta_imagen + '"/>' +
+					'<div class="nombrelist">' + evento.nombre + '</div>' +
+					'<p>' + evento.fechaevento + '</p>');
+		});
+		$('#eventosList').listview('refresh');
+	});
+}
+
+
+function getOcasionList() {
+	mostrarOcasion();
+	$.getJSON(serviceocasionURL, function(data) {
+		$('#ocasionList li').remove();
+		eventos = data;
+		$.each(eventos, function(index, ocasion) {
+			$('#ocasionList').append('<li><a href="ocasiondetails.html?id=' + ocasion.id_casco + '">' +
+					'<img src="' + ocasion.ruta_imagen + '"/>' +
+					'<h4>' + ocasion.modelo + '</h4>' +
+					'<p>' + ocasion.kilometros + ' kil&oacute;metros - ' + ocasion.precio + '</p></li>');
+		});
+		$('#ocasionList').listview('refresh');
+	});
+}
+
+function mostrarOfertas()
+{
+  $('#apptitle').text('Ofertas');
+  $('#contenedoreventos').hide();
+  $('#contenedorofertas').show();
+  $('#contenedorocasion').hide();
+}
+
+
+function mostrarEventos()
+{
+  $('#apptitle').text('Eventos');
+  $('#contenedoreventos').show();
+  $('#contenedorofertas').hide();
+  $('#contenedorocasion').hide();
+}
+
+function mostrarOcasion()
+{
+  $('#apptitle').text('');
+  $('#apptitle').append('Ocasi&oacute;n');
+  $('#contenedoreventos').hide();
+  $('#contenedorofertas').hide();
+  $('#contenedorocasion').show();
 }
